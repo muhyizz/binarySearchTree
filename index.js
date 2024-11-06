18a9d9d4d0313f692d1757d2fd091ac9bbaec5e0
muhyizz <muhyizz.mohammed@gmail.com>
2024-11-07 01:35:40 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,139 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  isEmpty() {
+    return this.root === null;
+  }
+
+  insert(value) {
+    const newNode = new Node(value);
+    if (this.isEmpty()) {
+      this.root = newNode;
+    } else {
+      this.insertNode(this.root, newNode);
+    }
+  }
+
+  insertNode(root, newNode) {
+    if (newNode.value < root.value) {
+      if (root.left === null) {
+        root.left = newNode;
+      } else {
+        this.insertNode(root.left, newNode);
+      }
+    } else {
+      if (root.right === null) {
+        root.right = newNode;
+      } else {
+        this.insertNode(root.right, newNode);
+      }
+    }
+  }
+
+  search(root, value) {
+    if (!root) {
+      return false;
+    } else {
+      if (root.value === value) {
+        return true;
+      } else if (value < root.value) {
+        return this.search(root.left, value);
+      } else {
+        return this.search(root.right, value);
+      }
+    }
+  }
+
+  preOrder(root) {
+    if (root) {
+      console.log(root.value);
+      this.preOrder(root.left);
+      this.preOrder(root.right);
+    }
+  }
+
+  inOrder(root) {
+    if (root) {
+      this.inOrder(root.left);
+      console.log(root.value);
+      this.inOrder(root.right);
+    }
+  }
+
+  postOrder(root) {
+    if (root) {
+      this.postOrder(root.left);
+      this.postOrder(root.right);
+      console.log(root.value);
+    }
+  }
+
+  levelOrder() {
+    const queue = [];
+    queue.push(this.root);
+    console.log(queue);
+
+    while (queue.length) {
+      const currentNode = queue.shift(); //take the head of the array and put into the current queue
+      console.log(currentNode.value);
+
+      if (currentNode.left !== null) {
+        queue.push(currentNode.left);
+      }
+      if (currentNode.right !== null) {
+        queue.push(currentNode.right);
+      }
+    }
+  }
+}
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.value}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+};
+
+const bst = new BinarySearchTree();
+console.log("Tree is empty?", bst.isEmpty());
+
+bst.insert(10);
+bst.insert(5);
+bst.insert(15);
+bst.insert(3);
+bst.insert(7);
+
+console.log(bst.search(bst.root, 5));
+bst.preOrder(bst.root);
+bst.inOrder(bst.root);
+bst.postOrder(bst.root);
+
+// console.log("Root:", bst.root ? bst.root.value : "undefined"); // Check root value
+// console.log(
+//   "Root Left Child:",
+//   bst.root.left ? bst.root.left.value : "undefined"
+// ); // Check left child
+// console.log(
+//   "Root Right Child:",
+//   bst.root.right ? bst.root.right.value : "undefined"
+// ); // Check right child
+
+prettyPrint(bst.root);
+
+bst.levelOrder();
